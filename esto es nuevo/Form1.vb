1d2e27ab417507fe7a8ea3bdfe1984fca797b51b
ANDRES LEANDRO LOPEZ RAMIREZ <andres.lopez28@inacapmail.cl>
2018-12-25 01:45:45 -0300

    nuevo abanse

--- esto es nuevo/Form1.vb
+++ esto es nuevo/Form1.vb
@@ -1,3 +1,18 @@
 ﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+
+
+        Dim edad As Integer = 19
+        Dim anos As Date = "03-05-1999"
+        Dim nombre As String = "andres "
+        MsgBox(nombre.ToString + ("Edad: ") + edad.ToString + ("  Fecha de nacimiento:  ") + anos.ToString)
+
+
+
+
+
+
+
+    End Sub
 End Class
